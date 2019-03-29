5df8d3a0682e56509a08c729397da16612164cb0
Cimbi <Cimbi@DESKTOP-0OTU7V4>
2019-03-29 17:48:57 +0100

    Added Door class andd Key class

---
+++ ConsoleMario/Devices/Door.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleMario.Devices
+{
+    class Door : Device
+    {
+        // Describes if the Door is opened
+        public bool Opened { get; set; } = false;
+        public Door() : base('¤') {}
+
+        public override void Use(Player player)
+        {
+            // if the door is closed
+            if (!Opened)
+            {
+                throw new DoorIsClosedException();
+            }
+        }
+    }
+    class DoorIsClosedException : Exception
+    {
+        public DoorIsClosedException() : base("The Door, which on you are is closed!") { }
+    }
+}
---
+++ ConsoleMario/Devices/Key.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleMario.Devices
+{
+    class Key : Device
+    {
+        // Door to open
+        readonly Door door;
+        public Key(Door _door) : base('@') { this.door = _door; }
+
+        public override void Use(Player player)
+        {
+            // opens the door
+            this.door.Opened = true;
+        }
+    }
+}
