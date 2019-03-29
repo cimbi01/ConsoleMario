e2b620f1427d3c4597f01127ba4aea7db998384a
Cimbi <Cimbi@DESKTOP-0OTU7V4>
2019-03-29 17:05:02 +0100

    Device class declaration

---
+++ ConsoleMario/Device.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleMario
+{
+    abstract class Device
+    {
+        public string Character { get; }
+        public abstract void Use(Player player);
+    }
+}
