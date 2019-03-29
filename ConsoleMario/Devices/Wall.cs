88599474fbaef9d3c2b86c80630782817366aba2
Cimbi <Cimbi@DESKTOP-0OTU7V4>
2019-03-29 17:38:32 +0100

    Added Wall class

---
+++ ConsoleMario/Devices/Wall.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleMario.Devices
+{
+    public class Wall : Device
+    {
+        public Wall() : base('*') { }
+        // throws a new RunInWallException
+        public override void Use(Player player)
+        {
+            throw new RunInWallException();
+        }
+    }
+    // this exception throwned when player run in a wall
+    public class RunInWallException : Exception
+    {
+        public RunInWallException() : base("You've run in a wall") { }
+    }
+}
