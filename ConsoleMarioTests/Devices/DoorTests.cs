628929de03bb14ec15c9ba4ca4a731d0d0faa54b
Cimbi <Cimbi@DESKTOP-0OTU7V4>
2019-03-29 17:55:12 +0100

    Successfull KeyTest and DoorTest

---
+++ ConsoleMarioTests/Devices/DoorTests.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleMario.Devices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleMario.Devices.Tests
+{
+    [TestClass()]
+    public class DoorTests
+    {
+        [TestMethod()]
+        [ExpectedException(typeof(DoorIsClosedException))]
+        public void UseTestLocked()
+        {
+            Player player = new Player();
+            Door door = new Door();
+            Key key = new Key(door);
+            door.Use(player);
+        }
+        [TestMethod()]
+        public void UseTestUnlocked()
+        {
+            Player player = new Player();
+            Door door = new Door();
+            Key key = new Key(door);
+            key.Use(player);
+            door.Use(player);
+            Assert.AreEqual(true, door.Opened);
+        }
+    }
+}
---
+++ ConsoleMarioTests/Devices/KeyTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleMario.Devices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleMario.Devices.Tests
+{
+    [TestClass()]
+    public class KeyTests
+    {
+        [TestMethod()]
+        public void UseTestUnlocked()
+        {
+            Player player = new Player();
+            Door door = new Door();
+            Key key = new Key(door);
+            key.Use(player);
+            door.Use(player);
+            Assert.AreEqual(true, door.Opened);
+        }
+    }
+}
