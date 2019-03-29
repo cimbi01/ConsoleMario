fe5ba1f53a18d19c20c10ef2cd7525e9a9261481
Cimbi <Cimbi@DESKTOP-0OTU7V4>
2019-03-29 16:30:24 +0100

    Player

---
+++ ConsoleMario/Player.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleMario
+{
+    class Player
+    {
+        // up, down, righ left keyboard key init
+        private static readonly char UP = 'w', DOWN = 's', RIGHT = 'a', LEFT = 'd';
+        // Character live
+        public bool Live { get; private set; } = true;
+        // Character character on console to write
+        public char Character { get; } = '+';
+        // Character position X,Y on console to write
+        // row
+        public int PositionX { get; private set; } = 1;
+        //column
+        public int PositionY { get; private set; } = 1;
+        // and previous X,Y on console
+        //previous row
+        public int PreviousPositionX { get; private set; } = 1;
+        //previous column
+        public int PreviousPositionY { get; private set; } = 1;
+        // move x, y positions
+        // DOESNT HANDLE UPPERCASES!!! :(
+        public void Move(char ch)
+        {
+            PreviousPositionX = PositionX;
+            PreviousPositionY = PositionY;
+            if (ch == UP)
+            { PositionX--; }
+            else if (ch == DOWN)
+            { PositionX++; }
+            else if (ch == RIGHT)
+            { PositionY++; }
+            else if (ch == LEFT)
+            { PositionY--; }
+            // THROW NOTUSEDKEYEXCEPTION
+            else
+            { }
+        }
+    }
+}
