ba9a68b743753b9765f2eb3af6252bd8db95dfb9
Cimbi <Cimbi@DESKTOP-0OTU7V4>
2019-03-29 17:15:17 +0100

    Moved UnitTest, and player to new Tests solution Created FireTest > UseTest() Successfully tested

---
+++ ConsoleMarioTests/Devices/FireTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleMario.Devices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleMario.Devices.Tests
+{
+    [TestClass()]
+    public class FireTests
+    {
+        [TestMethod()]
+        public void UseTest()
+        {
+            Player player = new Player();
+            Fire fire = new Fire();
+            fire.Use(player);
+            Assert.AreEqual(false, player.Live);
+        }
+    }
+}
