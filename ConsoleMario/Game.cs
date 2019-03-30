3e90e7f7b01a883ea797683dfedd64c732fd914b
Cimbi <Cimbi@DESKTOP-0OTU7V4>
2019-03-30 22:07:36 +0100

    Simplified Code By Adding Render class that handles the Console

--- ConsoleMario/Game.cs
+++ ConsoleMario/Game.cs
@@ -9,90 +9,60 @@ namespace ConsoleMario
 {
     public static class Game
     {
-        private static bool messages_visible = false;
         // List of messages by Pathlevel
-        private static List<string> messages = new List<string>();
-        // default Console Background Color and Default Color to change on cursor
-        private static readonly ConsoleColor default_bgcolor = ConsoleColor.Black;
-        private static readonly ConsoleColor change_bgcolor = ConsoleColor.DarkRed;
+        public static List<string> Messages { get; } = new List<string>();
         // Describes that player want to exit
         private static bool exited = false;
         // Describes all the existed Paths returned by path.init
         private static readonly List<Path> paths = Path.Init();
         // Describes the player
-        private static readonly Player player = new Player();
-        // Describes the max of levels of the player won -1
-        private static int player_maxLevel = 0;
+        public static Player Player { get; } = new Player();
+        // Describes the max of levels of the player won
+        public static int player_maxLevel = 0;
         // Describes the actual level
         private static int actual_level = 0;
         // Describes the actual path
         private static Path actual_path;
-        // Describes the render path
-        private static Path renderpath;
         public static void Play()
         {
-            // cursor will not be visible
-            Console.CursorVisible = false;
-            // while player does not want to exit
             while (!exited)
             {
-                player.Reset();
+                Player.Reset();
                 actual_path = new Path(paths[actual_level]);
-                Console.Clear();
-                // if the player win the example path of the actual_path
-                // or if the actual path has no example path
-                // then render the actual_path
-                if (player.ExamplePathWin || actual_path.ExamplePath == null)
+                Render.Renderpath = actual_path;
+                if (!Player.ExamplePathWin && actual_path.ExamplePath != null)
                 {
-                    renderpath = actual_path;
+                    Render.Renderpath = actual_path.ExamplePath;
                 }
-                // if isnt won &&& has example path
-                // then render the example path of it
-                else
-                {
-                    renderpath = actual_path.ExamplePath;
-                }
-                RenderRenderPath();
-                // while player doesnt win and player is alive
-                while (!player.Win && player.Life > 0)
+                Render.RenderRenderPath();
+                while (!Player.Win && Player.Life > 0)
                 {
                     Move();
                 }
-                // if player won
-                if (player.Win)
+                if (Player.Win)
                 {
-                    Console.Clear();
-                    string wintext = "You Win!";
-                    if (renderpath is ExamplePath)
+                    Render.WriteWinMessage();
+                    if (!(Render.Renderpath is ExamplePath))
                     {
-                        wintext += "\nWelldone!\nYou can move to the actual Path from the example!";
+                        if (actual_level == player_maxLevel && player_maxLevel < Path.MaxLevel)
+                        {
+                            player_maxLevel++;
+                            AddNewMessageLine();
+                        }
+                        if (actual_level < Path.MaxLevel)
+                        {
+                            actual_level++;
+                            Player.ExamplePathWin = false;
+                        }
                     }
-                    Console.WriteLine(wintext);
-                    // if actual level = player_maxlevel
-                    // && if the rendered path is not Example path
-                    // && if playermaxlevel < path.maxlevel 
-                    // then player_maxlevel++
-                    if (actual_level == player_maxLevel && player_maxLevel < Path.MaxLevel && !(renderpath is ExamplePath))
+                    else
                     {
-                        player_maxLevel++;
-                        messages.Add("");
-                    }
-                    // if actual level < Path.MaxLevel && the renderpath is not examplepath
-                    // then actual level++ cause there is more level above
-                    if (actual_level < Path.MaxLevel && !(renderpath is ExamplePath))
-                    {
-                        actual_level++;
-                        player.ExamplePathWin = false;
-                    }
-                    else if (renderpath is ExamplePath)
-                    {
-                        player.ExamplePathWin = true;
+                        Player.ExamplePathWin = true;
                     }
                 }
                 else
                 {
-                    Console.Clear();
-                    Console.WriteLine("You Lost");
+                    Render.WriteWinMessage(false);
                 }
                 System.Threading.Thread.Sleep(1000);
                 exited = Path.MaxLevel == player_maxLevel;
@@ -100,121 +70,72 @@ namespace ConsoleMario
             // if player want to exit
             Console.WriteLine("Game over");
         }
-        static Game()
-        {
-            for (int i = 0; i < player_maxLevel+1; i++)
-            {
-                messages.Add("");
-            }
-        }
-        // Write the Actual Path but on x, y position write the player character
-        private static void RenderRenderPath()
-        {
-            for (int i = 0; i < renderpath.Devices.GetLength(0); i++)
-            {
-                for (int j = 0; j < renderpath.Devices.GetLength(1); j++)
-                {
-                    if (i == player.PositionX && j == player.PositionY)
-                    {
-                        Console.Write(player.Character);
-                    }
-                    else
-                    {
-                        Console.Write(renderpath.Devices[i, j].Character);
-                    }
-                }
-                Console.WriteLine();
-            }
-            // if the path is Example Path
-            // then Write The Preview
-            if (renderpath is ExamplePath)
-            {
-                Console.WriteLine();
-                Console.WriteLine((renderpath as ExamplePath).Preview);
-                Console.WriteLine();
-            }
-            // write all the messages by the Path
-            AddMessage(messages[actual_level], false);
-        }
         private static void Move()
         {
             char ch = Console.ReadKey(true).KeyChar;
-            player.Move(ch);
-            AddMessage("Playered moved x Direction: " + Convert.ToString(player.PositionX-player.PreviousPositionX) +
-                " y Direction: " + Convert.ToString(player.PositionY - player.PreviousPositionY));
+            Player.Move(ch);
+            AddMessage("Player moved x Direction: " + Convert.ToString(Player.PositionX-Player.PreviousPositionX) +
+                " y Direction: " + Convert.ToString(Player.PositionY - Player.PreviousPositionY));
             RenderPlayer();
         }
         // render player and use the player positionx, positiony device of actual_path devices
         private static void RenderPlayer()
         {
-            if (player.Life > 0)
+            if (Player.Life > 0)
             {
-                // setcursor to previus position and write the previous device where it is
-                Console.SetCursorPosition(player.PreviousPositionY, player.PreviousPositionX);
-                Console.Write(renderpath.Devices[player.PreviousPositionX, player.PreviousPositionY].Character);
-                // setcursor to current position and write the current device where it is
-                Console.BackgroundColor = change_bgcolor;
-                Console.SetCursorPosition(player.PositionY, player.PositionX);
-                Console.Write(renderpath.Devices[player.PositionX, player.PositionY].Character);
-                Console.BackgroundColor = default_bgcolor;
-                Console.SetCursorPosition(player.PositionY, player.PositionX);
+                Render.RenderPlayer();
                 System.Threading.Thread.Sleep(300);
                 try
                 {
-                    AddMessage("Device: " + Convert.ToString(renderpath.Devices[player.PositionX, player.PositionY].GetType()));
-                    renderpath.Devices[player.PositionX, player.PositionY].Use(player);
+                    AddMessage("Device: " + Convert.ToString(Render.Renderpath.Devices[Player.PositionX, Player.PositionY].GetType()));
+                    Render.Renderpath.Devices[Player.PositionX, Player.PositionY].Use(Player);
                 }
                 // if the door is closed 
                 catch (DoorIsClosedException)
                 {
                     AddMessage("Run in Closed Door");
-                    player.RenderNeeded = false;
+                    Player.RenderNeeded = false;
                     // step back and renderplayer again
-                    player.StepBack();
-                    AddMessage("Playered moved x Direction: " + Convert.ToString(player.PositionX - player.PreviousPositionX) +
-                        " y Direction: " + Convert.ToString(player.PositionY - player.PreviousPositionY));
+                    Player.StepBack();
+                    AddMessage("Player moved x Direction: " + Convert.ToString(Player.PositionX - Player.PreviousPositionX) +
+                        " y Direction: " + Convert.ToString(Player.PositionY - Player.PreviousPositionY));
                     RenderPlayer();
                 }
                 // if run in wall
                 catch (RunInWallException)
                 {
                     AddMessage("Run in Wall");
-                    player.RenderNeeded = false;
+                    Player.RenderNeeded = false;
                     // step back and renderplayer again
-                    player.StepBack();
-                    AddMessage("Playered moved x Direction: " + Convert.ToString(player.PositionX - player.PreviousPositionX) +
-                        " y Direction: " + Convert.ToString(player.PositionY - player.PreviousPositionY));
+                    Player.StepBack();
+                    AddMessage("Player moved x Direction: " + Convert.ToString(Player.PositionX - Player.PreviousPositionX) +
+                        " y Direction: " + Convert.ToString(Player.PositionY - Player.PreviousPositionY));
                     RenderPlayer();
                 }
                 // if player need rerender
-                if (player.RenderNeeded)
+                if (Player.RenderNeeded)
                 {
-                    AddMessage("Playered moved x Direction: " + Convert.ToString(player.PositionX - player.PreviousPositionX) +
-                        " y Direction: " + Convert.ToString(player.PositionY - player.PreviousPositionY));
-                    player.RenderNeeded = false;
+                    AddMessage("Player moved x Direction: " + Convert.ToString(Player.PositionX - Player.PreviousPositionX) +
+                        " y Direction: " + Convert.ToString(Player.PositionY - Player.PreviousPositionY));
+                    Player.RenderNeeded = false;
                     RenderPlayer();
                 }
             }
         }
         // Add message to messages and write it under Path
-        private static void AddMessage(string message, bool add = true)
+        public static void AddMessage(string message)
         {
-            if (add)
+            Messages[actual_level] += '\n' + message;
+            int x = Render.Renderpath.Column, y = Render.Renderpath.Row + 1;
+            if (Render.Renderpath is ExamplePath)
             {
-                messages[actual_level] += '\n' + message;
+                y += (Render.Renderpath as ExamplePath).Preview.Split('\n').Length;
             }
-            int x = renderpath.Column, y = renderpath.Row + 1;
-            // if renderpath is ExamplePath then add to y the number of rows of the preview
-            if (renderpath is ExamplePath)
-            {
-                y += (renderpath as ExamplePath).Preview.Split('\n').Length;
-            }
-            if (messages_visible)
-            {
-                Console.SetCursorPosition(x, y);
-                Console.Write(messages[actual_level]);
-                Console.SetCursorPosition(player.PositionY, player.PositionX);
-            }
+            Render.RenderMessages();
+        }
+        public static void AddNewMessageLine()
+        {
+            Messages.Add(Convert.ToString((player_maxLevel+1)) + ".level\n");
         }
     }
 }
---
+++ ConsoleMario/Render.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleMario
+{
+    class Render
+    {
+        private static bool Messages_Visible { get; set; } = true;
+        // default Console Background Color and Default Color to change on cursor
+        private static readonly ConsoleColor default_bgcolor = ConsoleColor.Black;
+        private static readonly ConsoleColor change_bgcolor = ConsoleColor.DarkRed;
+        // Describes the render path
+        public static Path Renderpath { set; get; }
+        static Render()
+        {
+            Console.CursorVisible = false;
+            for (int i = 0; i < Game.player_maxLevel+1; i++)
+            {
+                Game.Messages.Add(Convert.ToString((i + 1)) + ".level");
+            }
+        }
+        // Write the Actual Path but on x, y position write the player character
+        public static void RenderRenderPath()
+        {
+            Console.Clear();
+            for (int i = 0; i < Renderpath.Devices.GetLength(0); i++)
+            {
+                for (int j = 0; j < Renderpath.Devices.GetLength(1); j++)
+                {
+                    if (i == Game.Player.PositionX && j == Game.Player.PositionY)
+                    {
+                        Console.Write(Game.Player.Character);
+                    }
+                    else
+                    {
+                        Console.Write(Renderpath.Devices[i, j].Character);
+                    }
+                }
+                Console.WriteLine();
+            }
+            // if the path is Example Path
+            // then Write The Preview
+            if (Renderpath is ExamplePath)
+            {
+                Console.WriteLine();
+                Console.WriteLine((Renderpath as ExamplePath).Preview);
+                Console.WriteLine();
+            }
+            // write all the messages by the Path
+            RenderMessages();
+        }
+        // render player and use the player positionx, positiony device of actual_path devices
+        public static void RenderPlayer()
+        {
+            // setcursor to previus position and write the previous device where it is
+            Console.SetCursorPosition(Game.Player.PreviousPositionY, Game.Player.PreviousPositionX);
+            Console.Write(Renderpath.Devices[Game.Player.PreviousPositionX, Game.Player.PreviousPositionY].Character);
+            // setcursor to current position and write the current device where it is
+            Console.BackgroundColor = change_bgcolor;
+            Console.SetCursorPosition(Game.Player.PositionY, Game.Player.PositionX);
+            Console.Write(Renderpath.Devices[Game.Player.PositionX, Game.Player.PositionY].Character);
+            Console.BackgroundColor = default_bgcolor;
+            Console.SetCursorPosition(Game.Player.PositionY, Game.Player.PositionX);
+            RenderMessages();
+        }
+        public static void RenderMessages()
+        {
+            int x = 0,
+                y = Renderpath.Column + 1;
+            if(Messages_Visible)
+            {
+                if (Renderpath is ExamplePath)
+                {
+                    y += (Renderpath as ExamplePath).Preview.Split('\n').Length + 2;
+                }
+                Console.SetCursorPosition(x, y);
+                Console.Write(Game.Messages[Renderpath.LevelNumber-1]);
+                Console.SetCursorPosition(Game.Player.PositionY, Game.Player.PositionX);
+            }
+        }
+        public static void WriteWinMessage(bool win = true)
+        {
+            Console.Clear();
+            string text = "";
+            if (win)
+            {
+                text = "You Win!";
+                if (Renderpath is ExamplePath)
+                {
+                    text += "\nWelldone!\nYou can move to the actual Path from the example!";
+                }
+            }
+            else
+            {
+                text = "You lost!";
+            }
+            Console.WriteLine(text);
+        }
+    }
+}
